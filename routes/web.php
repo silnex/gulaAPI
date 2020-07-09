069fab943cc330649368a2984de5ce16c8527edf
silnex <silnex@silnex.kr>
2020-07-10 01:16:12 +0900

    게시판 별 rest full api 추가

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,23 @@
 <?php
 
+use App\G5Models\G5WriteFree;
+use App\G5Models\G5WriteGallery;
+use App\G5Models\G5WriteNotice;
+use App\G5Models\G5WriteQa;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/free', function () {
+    return G5WriteFree::all();
+});
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/qa', function () {
+    return G5WriteQa::all();
+});
+
+Route::get('/notice', function () {
+    return G5WriteNotice::all();
+});
+
+Route::get('/gallery', function () {
+    return G5WriteGallery::all();
 });
